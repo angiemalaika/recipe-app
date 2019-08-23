a066450008740a07deebe1a9606a817dfd1b9d3b
angiemalaika <malaika.mumba@gmail.com>
2019-08-22 23:12:53 -0400

    added favorites model

--- db/migrate/20190822002808_create_favorite_recipes.rb
+++ db/migrate/20190822002808_create_favorite_recipes.rb
@@ -1,6 +1,6 @@
-class CreateComments < ActiveRecord::Migration[5.2]
+class CreateFavoriteRecipes < ActiveRecord::Migration[5.2]
   def change
-    create_table :comments do |t|
+    create_table :favorite_recipes do |t|
       t.belongs_to :recipe, foreign_key: true
       t.belongs_to :user, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_21_031201) do
-
-  create_table "comments", force: :cascade do |t|
-    t.integer "recipe_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["recipe_id"], name: "index_comments_on_recipe_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2019_08_22_002808) do
 
   create_table "directions", force: :cascade do |t|
     t.text "step"
@@ -27,6 +18,15 @@ ActiveRecord::Schema.define(version: 2019_08_21_031201) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["recipe_id"], name: "index_directions_on_recipe_id"
+  end
+
+  create_table "favorite_recipes", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_favorite_recipes_on_recipe_id"
+    t.index ["user_id"], name: "index_favorite_recipes_on_user_id"
   end
 
   create_table "ingredients", force: :cascade do |t|
@@ -56,6 +56,8 @@ ActiveRecord::Schema.define(version: 2019_08_21_031201) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
